0398a50ca71dde102fa8a31e3547fb57d8071f71
Mantu105 <127162724+Mantu105@users.noreply.github.com>
2026-01-15 16:08:13 +0530

    Add files via upload
    developer mode on-off functionality work.

---
+++ toggle_developer_mode_on/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Toggle Debug Mode On",
+    'version': '19.0.0.0.1',
+    'category': 'Tools',
+    'author': "Mantu Raj",
+    'website': "https://www.linkedin.com/in/mantu105/",
+    'license': "AGPL-3",
+    'summary': """Toggle to debug mode in the top right user menu, just one click!""",
+    'description': """Toggle to debug mode in the top right user menu, just one click!""",
+    'depends': ['base','web',],
+    'assets': {
+        'web.assets_backend': [
+            'toggle_developer_mode_on/static/src/js/debug_mode_js.js',
+        ],
+    },
+    'installable': True,
+    'application': True,
+}
